757423a30fd6d1fd2f01fb2dc26fc5e654c3400b
sotreduch2602 <sotreduch26022001@gmail.com>
2025-03-29 11:51:12 +0700

    Add product listing layout to ProductsPage; enhance Tailwind configuration with new animations

--- src/pages/Productspage.jsx
+++ src/pages/Productspage.jsx
@@ -158,6 +158,52 @@ const ProductsPage = () => {
                 </div>
               </div>
             </div>
+            <div className="flex-1 pt-5">
+              <div className="h-[calc(100vh-160px)] overflow-y-auto pr-2 scrollbar-hide">
+                {/* FilteredProductList */}
+                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2.5">
+                  <div className="text-sm border-[1px] rounded-md border-sky-950/20 group bg-white">
+                    <div className="relative group overflow-hidden rounded-md bg-[#f6f6f6]">
+                      <a>
+                        <img
+                          className="w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 group-hover:scale-105"
+                          alt="productImage"
+                          loading="lazy"
+                          src="https://cdn.sanity.io/images/oe6m9gg8/production/1a4548882b7288ab4e9eebd38123cb18e704c701-500x500.png?rect=0,34,500,433&w=900&h=780&auto=format"
+                        ></img>
+                      </a>
+                    </div>
+                    <div className="p-3 flex flex-col gap-2">
+                      <p className="uppercase line-clamp-1 text-xs font-medium text-lightText">
+                        Television
+                      </p>
+                      <h2 className="font-semibold text-sm line-clamp-1">
+                        43″ Class TU7000 Series Crystal UHD 4K Smart TV
+                      </h2>
+                      <div className="flex items-center gap-2.5">
+                        <p className="font-medium">In Stock</p>
+                        <p className="text-shop_dark_green/80 font-semibold">
+                          6
+                        </p>
+                      </div>
+                      <div className="flex items-center justify-between gap-5">
+                        <div className="flex items-center gap-2">
+                          <span className="font-semibold text-red-600 text-sm">
+                            $1,599.00
+                          </span>
+                          <span className="line-through font-normal text-zinc-500 text-sm">
+                            $1,678.95
+                          </span>
+                        </div>
+                      </div>
+                      <div className="w-full h-12 flex items-center">
+                        <Button effect="gooeyLeft">Add to Cart</Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
