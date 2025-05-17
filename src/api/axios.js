8ea5666015f4d9b52d81b6c663971091cf335a9f
sotreduch2602 <sotreduch26022001.com>
2025-05-17 19:45:24 +0700

    update baseurl_local

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
-import BASE_URL from "./baseURL";
+import { BASE_URL, BASE_URL_LOCAL } from "./baseURL";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL_LOCAL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 export default axiosInstance;
--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://my-green-api-iugw.onrender.com";
+export const BASE_URL = "https://my-green-api-iugw.onrender.com";
+export const BASE_URL_LOCAL = "http://localhost:3000";
 
-export default BASE_URL;
