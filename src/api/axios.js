451b33e0f1c89fa6eab3e53e1292c683743e4dce
sotreduch2602 <sotreduch26022001.com>
2025-05-20 18:50:54 +0700

    update Deloyment

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL, BASE_URL_LOCAL } from "./baseURL";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL_LOCAL,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
